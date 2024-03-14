3ce1af3e13b39e734933010b1c5594dc3bd09a8a
arinata098 <arinata098@gmail.com>
2024-03-14 13:59:14 +0800

    first commit

---
+++ pages/products/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Products</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ad aperiam omnis placeat ducimus laudantium quisquam, repellendus ratione eum eveniet.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ad aperiam omnis placeat ducimus laudantium quisquam, repellendus ratione eum eveniet.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
