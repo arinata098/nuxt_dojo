58127b3a8357464ab8a3628b50f4ad8bcbca2cd5
arinata098 <arinata098@gmail.com>
2024-03-14 14:07:58 +0800

    route parameter

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde possimus eum architecto id, officiis deleniti perspiciatis dolore inventore numquam dolores.</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
