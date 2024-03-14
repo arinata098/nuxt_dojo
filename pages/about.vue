34a3edf9a451ec5b814cb2ef682b95b8e2726383
arinata098 <arinata098@gmail.com>
2024-03-14 14:52:46 +0800

    NuxtLink

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
+        <header>
+            <nav>
+                <ul>
+                    <nuxt-link to="/">Nuxt Dojo</nuxt-link>
+                       <li> <nuxt-link to="/">Home</nuxt-link> </li>
+                       <li> <nuxt-link to="/about">About</nuxt-link> </li>
+                       <li> <nuxt-link to="/products">Products</nuxt-link> </li>
+                </ul>
+            </nav>
+        </header>
         <h2>About</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ad aperiam omnis placeat ducimus laudantium quisquam, repellendus ratione eum eveniet.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ad aperiam omnis placeat ducimus laudantium quisquam, repellendus ratione eum eveniet.</p>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,5 +1,15 @@
 <template>
     <div>
+        <header>
+            <nav>
+                <ul>
+                    <nuxt-link to="/">Nuxt Dojo</nuxt-link>
+                       <li> <nuxt-link to="/">Home</nuxt-link> </li>
+                       <li> <nuxt-link to="/about">About</nuxt-link> </li>
+                       <li> <nuxt-link to="/products">Products</nuxt-link> </li>
+                </ul>
+            </nav>
+        </header>
         <p>Product details for {{ id }}</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde possimus eum architecto id, officiis deleniti perspiciatis dolore inventore numquam dolores.</p>
     </div>
